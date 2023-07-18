e082d435e1ca5e3736095e9bf4d52e88f6971ab3
stemensooh <stemensooh@gmail.com>
2023-07-18 00:57:01 -0500

    version inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { HeaderComponent } from './header/header.component';
+import { DatosComponent } from './datos/datos.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { EventsComponent } from './events/events.component';
+import { MapsComponent } from './maps/maps.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { FooterComponent } from './footer/footer.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    HeaderComponent,
+    DatosComponent,
+    GalleryComponent,
+    EventsComponent,
+    MapsComponent,
+    ReservationComponent,
+    FooterComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule
