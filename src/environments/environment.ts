0cfcbcd825c8474e88dfb2dd6b56cd447f31aeea
stemensooh <stemensooh@gmail.com>
2023-07-20 02:08:15 -0500

    config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://wedding-api.up.railway.app'
-    apiUrl: 'http://localhost:3000'
+    apiUrl: 'https://wedding-api.up.railway.app'
+    // apiUrl: 'http://localhost:3000'
 };
